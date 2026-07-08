f27745389a2e58f0ab32e4f828a2a042d95fe884
KarimBauer <82896159+KarimBauer@users.noreply.github.com>
2026-07-08 16:20:44 +0200

    Add file to track links

---
+++ src/config/links.ts
@@ -0,0 +1,10 @@
+const githubUsername = "karimbauer";
+
+const links = {
+  githubUsername,
+  github: `https://github.com/${githubUsername}`,
+  githubAvatar: `https://github.com/${githubUsername}.png`,
+  linkedin: "https://www.linkedin.com/in/karimbauer",
+};
+
+export default links;
